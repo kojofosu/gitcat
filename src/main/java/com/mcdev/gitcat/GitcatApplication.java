879272c0df495ad2bb1acab09e23a4b68ea35f76
kojofosu <kojofosube@gmail.com>
2021-03-01 15:17:14 +0000

    init

---
+++ src/main/java/com/mcdev/gitcat/GitcatApplication.java
@@ -0,0 +1,13 @@
+package com.mcdev.gitcat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitcatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitcatApplication.class, args);
+	}
+
+}
